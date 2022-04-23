a03b8b94b71e3530028d3874503557f7f2752824
olchh <98604592+olchh@users.noreply.github.com>
2022-04-23 14:58:17 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //В строке есть правильный ip-адрес. Необходимо написать приложение, которое его выведет на консоль.
+        System.out.println("введите ip: ");
+        Scanner in = new Scanner(System.in);
+        String wuw = in.nextLine();
+
+        Pattern p = Pattern.compile("\\b([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\b");
+        //установка шаблона
+
+        Matcher m = p.matcher(wuw);
+        //проводит поиск в строке
+
+        //матчер проводит тока поиск, а гроуп уже получает строку
+
+        if (m.find()) { //если есть подстрока, совпадающая с шаблоном
+            System.out.println("ip найден: " + m.group()); //получ нач последоват,сопоставленной с предыдущим результатом сопоставления.
+        }
+        else {
+            System.out.println("ip не найден");
+        }
+    }
+}
